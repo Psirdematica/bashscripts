beed1b35068c98382d9639de5533176fb0599b3c
Psirdem <setsoafiasedemjoseph1219@gmail.com>
2023-04-11 11:35:16 +0000

    bash script for the 4th video (terminal arguments) of the playlist

---
+++ 4.args.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# $1 $2 $3 $4 $# $@
+echo "YOu passed $# total args"
+echo "arg1 is : $1"
+echo "arg2 is : $2"
+echo "arg3 is : $3"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# var1=34
+# var2=a
+# var3=A
+# file="hey.txt"
+
+# if [[ -e $file && -s $file ]]
+# then
+#     echo "$var3 is less than $var2"
+# else
+#     echo "Go away"
+# fi
+
+
+
